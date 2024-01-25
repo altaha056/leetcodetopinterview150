132e5ffdd36491d613c990166d2ce5364a9f004a
Altaha <altahabinsyafriadi@gmail.com>
2024-01-25 08:40:24 +0700

    56. Merge Intervals.js

--- 56. Merge Intervals.js
+++ 56. Merge Intervals.js
@@ -1,7 +1,17 @@
 var merge = function (intervals) {
-  let qq = intervals.flat().sort((a, b) => a - b);
-  
-  console.log(qq);
+  intervals.sort((a, b) => a[0] - b[0]);
+  let ans = [intervals[0]];
+  for (let i = 1; i < intervals.length; i++) {
+    if (ans[ans.length - 1][1] >= intervals[i][0]) {
+      ans[ans.length - 1][1] = Math.max(
+        ans[ans.length - 1][1],
+        intervals[i][1]
+      );
+    } else {
+      ans.push(intervals[i]);
+    }
+  }
+  return ans;
 };
 let intervals = [
   [1, 3],
